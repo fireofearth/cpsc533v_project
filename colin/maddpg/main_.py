469795ac2654944a56adfbbb4a90232377eec8a5
Colin Chen <colinc@fastmail.com>
2021-12-12 16:33:48 -0800

    add maddpg; some modifications

---
+++ colin/maddpg/main_.py
@@ -0,0 +1,250 @@
+import os
+import time
+import argparse
+import random
+import threading
+
+from tqdm import tqdm
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+from maddpg.maddpg import MADDPG
+
+def get_args():
+    """Here are the param for the training"""
+    parser = argparse.ArgumentParser("Reinforcement Learning experiments for multiagent environments")
+    # Environment
+    parser.add_argument("--time-steps", type=int, default=2_000_000, help="number of time steps")
+    # 一个地图最多env.n个agents，用户可以定义min(env.n,num-adversaries)个敌人，剩下的是好的agent
+
+    # Environment settings
+    # assume using simple_tag_v2
+    #parser.add_argument("--scenario-name", type=str, default="simple_tag", help="name of the scenario script")
+    parser.add_argument("--max-episode-len", type=int, default=100, help="maximum episode length")
+    parser.add_argument("--num-adversaries", type=int, default=3, help="number of adversaries")
+    parser.add_argument("--num-good", type=int, default=1, help="number of 'good' agents")
+    parser.add_argument("--num-obstacles", type=int, default=1, help="number of 'good' agents")
+
+
+    # Core training parameters
+    parser.add_argument("--lr-actor", type=float, default=1e-4, help="learning rate of actor")
+    parser.add_argument("--lr-critic", type=float, default=1e-3, help="learning rate of critic")
+    parser.add_argument("--epsilon", type=float, default=0.1, help="epsilon greedy")
+    parser.add_argument("--noise_rate", type=float, default=0.1, help="noise rate for sampling from a standard normal distribution ")
+    parser.add_argument("--gamma", type=float, default=0.95, help="discount factor")
+    parser.add_argument("--tau", type=float, default=0.01, help="parameter for updating the target network")
+    parser.add_argument("--buffer-size", type=int, default=int(5e5), help="number of transitions can be stored in buffer")
+    parser.add_argument("--batch-size", type=int, default=256, help="number of episodes to optimize at the same time")
+    # Checkpointing
+    parser.add_argument("--save-dir", type=str, default="./model", help="directory in which training state and model should be saved")
+    parser.add_argument("--save-rate", type=int, default=1000, help="save model once every time this many episodes are completed")
+    parser.add_argument("--model-dir", type=str, default="", help="directory in which training state and model are loaded")
+
+    # Evaluate
+    parser.add_argument("--evaluate-episodes", type=int, default=10, help="number of episodes for evaluating")
+    parser.add_argument("--evaluate-episode-len", type=int, default=100, help="length of episodes for evaluating")
+    parser.add_argument("--evaluate", type=bool, default=False, help="whether to evaluate the model")
+    parser.add_argument("--evaluate-rate", type=int, default=1000, help="how often to evaluate model")
+    args = parser.parse_args()
+    return args
+
+def _make_env(args):
+    from multiagent.environment import MultiAgentEnv
+    import multiagent.scenarios as scenarios
+
+    # load scenario from script
+    scenario = scenarios.load(args.scenario_name + ".py").Scenario()
+
+    # create world
+    world = scenario.make_world()
+    # create multiagent environment
+    env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation)
+    # env = MultiAgentEnv(world)
+    args.n_players = env.n  # 包含敌人的所有玩家个数
+    args.n_agents = env.n - args.num_adversaries  # 需要操控的玩家个数，虽然敌人也可以控制，但是双方都学习的话需要不同的算法
+    args.obs_shape = [env.observation_space[i].shape[0] for i in range(args.n_agents)]  # 每一维代表该agent的obs维度
+    action_shape = []
+    for content in env.action_space:
+        action_shape.append(content.n)
+    args.action_shape = action_shape[:args.n_agents]  # 每一维代表该agent的act维度
+    args.high_action = 1
+    args.low_action = -1
+    return env, args
+
+def make_env(args):
+    from pettingzoo.mpe import simple_tag_v2
+    env = simple_tag_v2.env(
+        num_good=args.num_good,
+        num_adversaries=args.num_adversaries,
+        num_obstacles=args.num_obstacles,
+        max_cycles=args.max_episode_len,
+        continuous_actions=False
+    ).unwrapped
+    args.n_agents = args.num_good + args.num_adversaries
+    args.obs_shape = [env.observation_space[agent_name].shape[0] for agent_name in env.agents]
+    
+    
+
+class Agent:
+    def __init__(self, agent_id, args):
+        self.args = args
+        self.agent_id = agent_id
+        self.policy = MADDPG(args, agent_id)
+
+    def select_action(self, o, noise_rate, epsilon):
+        if np.random.uniform() < epsilon:
+            u = np.random.uniform(-self.args.high_action, self.args.high_action, self.args.action_shape[self.agent_id])
+        else:
+            inputs = torch.tensor(o, dtype=torch.float32).unsqueeze(0)
+            pi = self.policy.actor_network(inputs).squeeze(0)
+            # print('{} : {}'.format(self.name, pi))
+            u = pi.cpu().numpy()
+            noise = noise_rate * self.args.high_action * np.random.randn(*u.shape)  # gaussian noise
+            u += noise
+            u = np.clip(u, -self.args.high_action, self.args.high_action)
+        return u.copy()
+
+    def learn(self, transitions, other_agents):
+        self.policy.train(transitions, other_agents)
+
+class Buffer:
+    """ReplayBuffer"""
+
+    def __init__(self, args):
+        self.size = args.buffer_size
+        self.args = args
+        # memory management
+        self.current_size = 0
+        # create the buffer to store info
+        self.buffer = dict()
+        for i in range(self.args.n_agents):
+            self.buffer['o_%d' % i] = np.empty([self.size, self.args.obs_shape[i]])
+            self.buffer['u_%d' % i] = np.empty([self.size, self.args.action_shape[i]])
+            self.buffer['r_%d' % i] = np.empty([self.size])
+            self.buffer['o_next_%d' % i] = np.empty([self.size, self.args.obs_shape[i]])
+        # thread lock
+        self.lock = threading.Lock()
+
+    # store the episode
+    def store_episode(self, o, u, r, o_next):
+        idxs = self._get_storage_idx(inc=1)  # 以transition的形式存，每次只存一条经验
+        for i in range(self.args.n_agents):
+            with self.lock:
+                self.buffer['o_%d' % i][idxs] = o[i]
+                self.buffer['u_%d' % i][idxs] = u[i]
+                self.buffer['r_%d' % i][idxs] = r[i]
+                self.buffer['o_next_%d' % i][idxs] = o_next[i]
+    
+    # sample the data from the replay buffer
+    def sample(self, batch_size):
+        temp_buffer = {}
+        idx = np.random.randint(0, self.current_size, batch_size)
+        for key in self.buffer.keys():
+            temp_buffer[key] = self.buffer[key][idx]
+        return temp_buffer
+
+    def _get_storage_idx(self, inc=None):
+        inc = inc or 1
+        if self.current_size+inc <= self.size:
+            idx = np.arange(self.current_size, self.current_size+inc)
+        elif self.current_size < self.size:
+            overflow = inc - (self.size - self.current_size)
+            idx_a = np.arange(self.current_size, self.size)
+            idx_b = np.random.randint(0, self.current_size, overflow)
+            idx = np.concatenate([idx_a, idx_b])
+        else:
+            idx = np.random.randint(0, self.size, inc)
+        self.current_size = min(self.size, self.current_size+inc)
+        if inc == 1:
+            idx = idx[0]
+        return idx
+
+class Runner:
+    def __init__(self, args, env):
+        self.args = args
+        self.noise = args.noise_rate
+        self.epsilon = args.epsilon
+        self.episode_limit = args.max_episode_len
+        self.env = env
+        self.agents = self._init_agents()
+        self.buffer = Buffer(args)
+        self.save_path = self.args.save_dir + '/' + self.args.scenario_name
+        if not os.path.exists(self.save_path):
+            os.makedirs(self.save_path)
+
+    def _init_agents(self):
+        agents = []
+        for i in range(self.args.n_agents):
+            agent = Agent(i, self.args)
+            agents.append(agent)
+        return agents
+
+    def run(self):
+        returns = []
+        for time_step in tqdm(range(self.args.time_steps)):
+            # reset the environment
+            if time_step % self.episode_limit == 0:
+                s = self.env.reset()
+            u = []
+            actions = []
+            with torch.no_grad():
+                for agent_id, agent in enumerate(self.agents):
+                    action = agent.select_action(s[agent_id], self.noise, self.epsilon)
+                    u.append(action)
+                    actions.append(action)
+            for i in range(self.args.n_agents, self.args.n_players):
+                actions.append([0, np.random.rand() * 2 - 1, 0, np.random.rand() * 2 - 1, 0])
+            s_next, r, done, info = self.env.step(actions)
+            self.buffer.store_episode(s[:self.args.n_agents], u, r[:self.args.n_agents], s_next[:self.args.n_agents])
+            s = s_next
+            if self.buffer.current_size >= self.args.batch_size:
+                transitions = self.buffer.sample(self.args.batch_size)
+                for agent in self.agents:
+                    other_agents = self.agents.copy()
+                    other_agents.remove(agent)
+                    agent.learn(transitions, other_agents)
+            if time_step > 0 and time_step % self.args.evaluate_rate == 0:
+                returns.append(self.evaluate())
+                plt.figure()
+                plt.plot(range(len(returns)), returns)
+                plt.xlabel('episode * ' + str(self.args.evaluate_rate / self.episode_limit))
+                plt.ylabel('average returns')
+                plt.savefig(self.save_path + '/plt.png', format='png')
+            self.noise = max(0.05, self.noise - 0.0000005)
+            self.epsilon = max(0.05, self.epsilon - 0.0000005)
+            np.save(self.save_path + '/returns.pkl', returns)
+
+    def evaluate(self):
+        returns = []
+        for episode in range(self.args.evaluate_episodes):
+            # reset the environment
+            s = self.env.reset()
+            rewards = 0
+            for time_step in range(self.args.evaluate_episode_len):
+                self.env.render()
+                actions = []
+                with torch.no_grad():
+                    for agent_id, agent in enumerate(self.agents):
+                        action = agent.select_action(s[agent_id], 0, 0)
+                        actions.append(action)
+                for i in range(self.args.n_agents, self.args.n_players):
+                    actions.append([0, np.random.rand() * 2 - 1, 0, np.random.rand() * 2 - 1, 0])
+                s_next, r, done, info = self.env.step(actions)
+                rewards += r[0]
+                s = s_next
+                time.sleep(0.001)
+            returns.append(rewards)
+            print('Returns is', rewards)
+        return sum(returns) / self.args.evaluate_episodes
+
+if __name__ == '__main__':
+    # get the params
+    args = get_args()
+    env, args = make_env(args)
+    runner = Runner(args, env)
+    if args.evaluate:
+        returns = runner.evaluate()
+        print('Average returns is', returns)
+    else:
+        runner.run()
